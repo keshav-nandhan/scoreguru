de3f7c0f79b679e1cd3677ed951770c1b1026feb
keshav-nandhan <37911262+keshav-nandhan@users.noreply.github.com>
2020-01-14 21:41:34 +0530

    second commit

---
+++ livescoreapp/src/app/score.model.ts
@@ -0,0 +1,14 @@
+export interface IPosts {
+    matches: {
+    "unique_id":number,
+    "date":string,
+    "dateTimeGMT":string,
+    "team-1":string,
+    "team-2":string,
+    "winner_team":string,
+    "toss_winner_team":string,
+    "squad":boolean,
+    "matchStarted":string,
+    "type":string
+}
+}
--- livescoreapp/src/app/scoreapi.service.ts
+++ livescoreapp/src/app/scoreapi.service.ts
@@ -1,24 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
+import {Observable} from 'rxjs';
+import { map } from 'rxjs/operators';
+import { IPosts } from '../app/score.model';
 @Injectable({
   providedIn: 'root'
 })
+
 export class ScoreapiService {
 
   constructor(private http: HttpClient) { }
-  private _postsURL = "https://jsonplaceholder.typicode.com/posts";
-  public cricscore:any={};
+  private _postsURL = "https://cricapi.com/api/matches?apikey=6QuosyAP5bN5EaglBrhXXPdPsM23";
+  public scoreapiobject;
 
-  getPosts(){
-      return this.http
-          .get(this._postsURL)
-          .map((response: Response) => {
-console.log(response);
-          })
-          .catch(this.handleError);
-  }
+  getPosts():Observable<IPosts[]>{
+    return this.http.get<IPosts[]>(this._postsURL).pipe(map(x=>{console.log(x);return x;}));
+}
 
   private handleError(error: Response) {
       return Observable.throw(error.statusText);
